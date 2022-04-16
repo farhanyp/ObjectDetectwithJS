2ce7f72b0681855f4c2d09eee5f5876481db1f3b
farhan.yudha2016we@gmail.com <farhan.yudha2016we@gmail.com>
2022-04-16 22:28:27 +0700

    up

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,8 @@
-// Import dependencies
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
-// 1. TODO - Import required model here
-// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
-// 2. TODO - Import drawing utility here
-// e.g. import { drawRect } from "./utilities";
 import {drawRect} from "./utilities";
 
 
@@ -17,8 +12,6 @@ function App() {
 
   // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
-    // e.g. const net = await cocossd.load();
     const net = await cocossd.load();
     //  Loop and detect hands
     setInterval(() => {
@@ -46,16 +39,12 @@ function App() {
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
       const obj = await net.detect(video);
       console.log(obj);
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
-      // 5. TODO - Update drawing utility
-      // drawSomething(obj, ctx)  
       drawRect(obj,ctx);
     }
   };
